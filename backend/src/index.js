eba9794816d30f4c0d6a23755f173c949333285b
mfathur <mfathur17@gmail.com>
2024-03-11 11:09:44 +0700

    [be] add config file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,27 @@
 import express from "express";
 import http from "http";
+import configObj from "./config/index.js";
+import dotenv from "dotenv";
+import apis from "./apis/index.js";
+
+dotenv.config();
+const config = configObj[process.env.NODE_ENV];
 
 const app = express();
 app.server = http.createServer(app);
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send({
     status: "success",
     message: "Hello from task management backend app",
   });
 });
 
-const port = 3000;
+apis(app);
+
+const port = process.env.PORT || config.port;
 app.server.listen(port, () => {
   console.log(`Server is listening on PORT ${app.server.address().port}`);
 });
