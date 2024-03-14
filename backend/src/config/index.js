6d09f2eb779d034967aef2dab601dc082f082173
mfathur <mfathur17@gmail.com>
2024-03-14 07:30:37 +0700

    [be] fix cors

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -2,7 +2,7 @@ export default {
   development: {
     port: 3000,
     corsHeader: ["Link", "Content-Disposition"],
-    corsOrigin: "*",
+    corsOrigin: "http://localhost:5173",
     corsAllowedMethods: ["GET", "PUT", "DELETE", "POST"],
   },
 };
