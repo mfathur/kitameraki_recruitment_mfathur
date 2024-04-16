c344f6a872a61f145570ee53180ab782527b715c
mfathur <mfathur17@gmail.com>
2024-04-16 17:18:49 +0700

    [fe] add inline add field form

--- frontend/src/components/SpinButtonComponent.tsx
+++ frontend/src/components/SpinButtonComponent.tsx
@@ -1,6 +1,8 @@
 import { Label, SpinButton, useId } from "@fluentui/react-components";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import DroppableZone from "./DroppableZone";
+import { useDrop } from "react-dnd";
+import { FORM_TYPES } from "../utils/constants";
 
 type Props = {
   onDrop: (field: FormFieldMetadata) => void;
@@ -12,38 +14,42 @@ const SpinButtonComponent = ({ onDrop }: Props) => {
   const [showRightDroppableZone, setShowRightDroppableZone] = useState(false);
   const [showLeftDroppableZone, setShowLeftDroppableZone] = useState(false);
 
-  const handleMouseOver = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    // Get the x-coordinate of the mouse relative to the component
-    const mouseX = e.nativeEvent.offsetX;
+  const ref = useRef<HTMLDivElement>(null);
 
-    const componentWidth = e.currentTarget.clientWidth;
+  const [, drop] = useDrop({
+    accept: [FORM_TYPES.DATE, FORM_TYPES.SPIN, FORM_TYPES.TEXT],
+    hover(_, monitor) {
+      if (!ref.current) {
+        return;
+      }
 
-    const isOnRight = mouseX > componentWidth / 2;
-    if (isOnRight) {
-      // show right droppable zone
-      setShowRightDroppableZone(true);
-      setShowLeftDroppableZone(false);
-    } else {
-      // show left droppable zone
-      setShowRightDroppableZone(false);
-      setShowLeftDroppableZone(true);
-    }
-  };
+      const hoveredArea = ref.current.getBoundingClientRect();
+      const hoverMiddleX = (hoveredArea.right - hoveredArea.left) / 2;
+      const mousePosition = monitor.getClientOffset();
+      const hoverClientX = mousePosition!.x - hoveredArea.left;
 
-  const handleMouseLeave = () => {
-    // hide right&left droppableZone
-    setShowLeftDroppableZone(false);
-    setShowRightDroppableZone(false);
-  };
+      if (hoverMiddleX < hoverClientX) {
+        setShowLeftDroppableZone(false);
+        setShowRightDroppableZone(true);
+      } else {
+        setShowLeftDroppableZone(true);
+        setShowRightDroppableZone(false);
+      }
+    },
+    collect: (monitor) => {
+      if (!monitor.isOver()) {
+        setShowLeftDroppableZone(false);
+        setShowRightDroppableZone(false);
+      }
+    },
+  });
+
+  drop(ref);
 
   return (
-    <div className="flex gap-x-4 items-end">
+    <div ref={ref} className="flex gap-x-4 items-end">
       {showLeftDroppableZone ? <DroppableZone onDrop={onDrop} /> : null}
-      <div
-        className="w-full"
-        onMouseOver={handleMouseOver}
-        onMouseLeave={handleMouseLeave}
-      >
+      <div className="w-full">
         <Label htmlFor={id} className="block ">
           Default SpinButton
         </Label>
